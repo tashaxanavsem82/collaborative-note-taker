ff6aefdea35c0e3b7ccb46bb19bcb27a1e50eca9
tashaxanavsem82 <swgwull@hotmail.com>
2025-08-31 03:50:25 -0500

    feat: initial commit for client/src/App.js

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Collaborative Note Taker</h1>
+      <p>Welcome to the Collaborative Note Taker!</p>
+    </div>
+  );
+}
+
+export default App;
